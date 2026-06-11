39dc91fc3087ff04b01814261f89b39682341bd2
OHENEBA-BENEDICTA <ohenebabenedicta809@gmail.com>
2026-06-11 15:43:27 +0000

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,13 +1,10 @@
 public class Triangle {
      public static void main(String[] args){
-          System.out.println("        T");
-          System.out.println("       TT");
-          System.out.println("      TTT");
-          System.out.println("     TTTT");
-          System.out.println("    TTTTT");
-          System.out.println("   TTTTTT");
-          System.out.println("  TTTTTTT");
-          System.out.println(" TTTTTTTT");
-          System.out.println("TTTTTTTTT");
-}
+          System.out.println("        T  ");
+          System.out.println("       TTT");
+          System.out.println("      TTTTT");
+          System.out.println("     TTTTTTT");
+          System.out.println("    TTTTTTTTT");
+          System.out.println("   TTTTTTTTTTTTT");
+     }
 }
