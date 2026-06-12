56a2c97a017f6761de4105a960551de349335af4
OHENEBA BENEDICTA <ohenebabenedicat809@gmail.com>
2026-06-11 22:20:27 -0700

    Added TableAndChairs and Triangle

---
+++ Triangle.java
@@ -0,0 +1,13 @@
+public class Triangle {
+     public static void main(String[] args){
+          System.out.println("        T");
+          System.out.println("       TT");
+          System.out.println("      TTT");
+          System.out.println("     TTTT");
+          System.out.println("    TTTTT");
+          System.out.println("   TTTTTT");
+          System.out.println("  TTTTTTT");
+          System.out.println(" TTTTTTTT");
+          System.out.println("TTTTTTTTT");
+}
+}
